849aea807b8d046fb2eb7b6e10d2573ba6ae1996
VAISHNAV <vabanave@gmail.com>
2026-08-16 18:39:29 +0530

    Add Singleton pattern implementation and update project configuration

---
+++ src/main/java/org/example/creational_patterns/Singleton.java
@@ -0,0 +1,19 @@
+package org.example.creational_patterns;
+
+public class Singleton {
+
+    public static volatile Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
